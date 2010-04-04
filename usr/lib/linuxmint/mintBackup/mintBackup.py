c24601956df4cb11a9503223dbda382e37acf535
Ikey Doherty <contactjfreak@googlemail.com>
2010-04-04 18:38:38 +0100

    Added GPL license header to mintBackup.py

--- usr/lib/linuxmint/mintBackup/mintBackup.py
+++ usr/lib/linuxmint/mintBackup/mintBackup.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 
+# mintBackup - A GUI backup and restoration utility
+# Author: Ikey Doherty <contactjfreak@googlemail.com>
+# Several parts of this program originate from the original
+# mintDesktop code by Clement Lefebvre <root@linuxmint.com>
+# Those parts are the "MessageDialog" class, the add_folder_exclude,
+# remove_exclude and add_file_exclude methods (although somewhat modified)
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
 try:
 	import pygtk
 	pygtk.require("2.0")
