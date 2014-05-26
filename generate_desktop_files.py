1ebec837e35f283b8b151df960536abff4ee1f5c
Frédéric Gaudet <fred@linuxmint.com>
2014-05-26 11:30:03 +0200

    KDE desktop file generation

--- generate_desktop_files.py
+++ generate_desktop_files.py
@@ -20,6 +20,7 @@ Terminal=false
 Type=Application
 Encoding=UTF-8
 Categories=Application;System;Settings
+NotShowIn=KDE
 """
 
 additionalfiles.generate(DOMAIN, PATH, "usr/share/applications/mintBackup.desktop", prefix, _("Backup Tool"), _("Make a backup of your home directory"), suffix)
@@ -40,3 +41,16 @@ MimeType=application/x-backup;
 additionalfiles.generate(DOMAIN, PATH, "usr/share/applications/mintBackup_mime.desktop", prefix, _("Backup Tool"), _("Make a backup of your home directory"), suffix)
 
 
+prefix = "[Desktop Entry]\n"
+
+suffix= """Exec=mintBackup
+Icon=/usr/lib/linuxmint/mintBackup/icon.png
+Terminal=false
+Type=Application
+Encoding=UTF-8
+Categories=Qt;KDE;System;
+X-KDE-StartupNotify=false
+OnlyShowIn=KDE
+"""
+
+additionalfiles.generate(DOMAIN, PATH, "usr/share/applications/kde4/mintBackup.desktop", prefix, _("Backup Tool"), _("Make a backup of your home directory"),  suffix, genericName=_("Make a backup of your home directory"))
