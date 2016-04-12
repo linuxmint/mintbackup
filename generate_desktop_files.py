166380775965ef04816bb26c5a843190805040be
Daniel Alley <dralley@ncsu.edu>
2016-04-11 22:34:42 -0400

    Moved icons to /usr/share, renamed folders from camelCase to lowercase

--- generate_desktop_files.py
+++ generate_desktop_files.py
@@ -15,7 +15,7 @@ gettext.install(DOMAIN, PATH)
 prefix = "[Desktop Entry]\n"
 
 suffix = """Exec=mintbackup
-Icon=/usr/lib/linuxmint/mintBackup/icon.svg
+Icon=/usr/share/pixmaps/mintbackup/icon.svg
 Terminal=false
 Type=Application
 Encoding=UTF-8
@@ -23,13 +23,13 @@ Categories=Application;System;Settings
 NotShowIn=KDE;
 """
 
-additionalfiles.generate(DOMAIN, PATH, "usr/share/applications/mintBackup.desktop", prefix, _("Backup Tool"), _("Make a backup of your home directory"), suffix)
+additionalfiles.generate(DOMAIN, PATH, "usr/share/applications/mintbackup.desktop", prefix, _("Backup Tool"), _("Make a backup of your home directory"), suffix)
 
 
 prefix = "[Desktop Entry]\n"
 
 suffix = """Exec=mintBackup
-Icon=/usr/lib/linuxmint/mintBackup/icon.png
+Icon=/usr/share/pixmaps/mintbackup/icon.png
 Terminal=false
 Type=Application
 Encoding=UTF-8
@@ -38,13 +38,13 @@ NoDisplay=true
 MimeType=application/x-backup;
 """
 
-additionalfiles.generate(DOMAIN, PATH, "usr/share/applications/mintBackup_mime.desktop", prefix, _("Backup Tool"), _("Make a backup of your home directory"), suffix)
+additionalfiles.generate(DOMAIN, PATH, "usr/share/applications/mintbackup_mime.desktop", prefix, _("Backup Tool"), _("Make a backup of your home directory"), suffix)
 
 
 prefix = "[Desktop Entry]\n"
 
 suffix = """Exec=mintBackup
-Icon=/usr/lib/linuxmint/mintBackup/icon.png
+Icon=/usr/share/pixmaps/mintbackup/icon.png
 Terminal=false
 Type=Application
 Encoding=UTF-8
@@ -53,4 +53,4 @@ X-KDE-StartupNotify=false
 OnlyShowIn=KDE;
 """
 
-additionalfiles.generate(DOMAIN, PATH, "usr/share/applications/kde4/mintBackup.desktop", prefix, _("Backup Tool"), _("Make a backup of your home directory"), suffix, genericName=_("Make a backup of your home directory"))
+additionalfiles.generate(DOMAIN, PATH, "usr/share/applications/kde4/mintbackup.desktop", prefix, _("Backup Tool"), _("Make a backup of your home directory"), suffix, genericName=_("Make a backup of your home directory"))
--- usr/lib/linuxmint/mintbackup/mintbackup.py
+++ usr/lib/linuxmint/mintbackup/mintbackup.py
@@ -27,7 +27,7 @@ except Exception as detail:
 gettext.install("mintbackup", "/usr/share/linuxmint/locale")
 
 HOME = os.path.expanduser("~")
-UI_FILE = '/usr/share/linuxmint/mintBackup/mintBackup.ui'
+UI_FILE = '/usr/share/linuxmint/mintbackup/mintbackup.ui'
 
 
 class TarFileMonitor():
@@ -122,7 +122,7 @@ class MintBackup:
     def __init__(self):
         self.builder = Gtk.Builder()
         self.builder.add_from_file(UI_FILE)
-        self.builder.get_object("main_window").set_icon_from_file("/usr/lib/linuxmint/mintBackup/icon.png")
+        self.builder.get_object("main_window").set_icon_from_file("/usr/share/pixmaps/mintbackup/icon.png")
 
         # handle command line filenames
         if len(sys.argv) > 1:
@@ -300,10 +300,10 @@ class MintBackup:
         self.builder.get_object("label_detail3").set_markup("<small>" + _("Restore a previous backup") + "</small>")
         self.builder.get_object("label_detail4").set_markup("<small>" + _("Restore previously installed applications") + "</small>")
 
-        self.builder.get_object("image_backup_data").set_from_file("/usr/lib/linuxmint/mintBackup/backup-data.svg")
-        self.builder.get_object("image_restore_data").set_from_file("/usr/lib/linuxmint/mintBackup/restore-data.svg")
-        self.builder.get_object("image_backup_software").set_from_file("/usr/lib/linuxmint/mintBackup/backup-software.svg")
-        self.builder.get_object("image_restore_software").set_from_file("/usr/lib/linuxmint/mintBackup/restore-software.svg")
+        self.builder.get_object("image_backup_data").set_from_file("/usr/share/pixmaps/mintbackup/backup-data.svg")
+        self.builder.get_object("image_restore_data").set_from_file("/usr/share/pixmaps/mintbackup/restore-data.svg")
+        self.builder.get_object("image_backup_software").set_from_file("/usr/share/pixmaps/mintbackup/backup-software.svg")
+        self.builder.get_object("image_restore_software").set_from_file("/usr/share/pixmaps/mintbackup/restore-software.svg")
 
         # i18n - Page 1 (choose backup directories)
         self.builder.get_object("label_title_destination").set_markup("<big><b>" + _("Backup files") + "</b></big>")
@@ -1441,7 +1441,7 @@ class MintBackup:
                 l = l.rstrip("\r\n")
                 l = l.split(" ")
                 self.blacklist.append(l[2])
-            bl = open("/usr/lib/linuxmint/mintBackup/software-selections.list", "r")
+            bl = open("/usr/share/linuxmint/mintbackup/software-selections.list", "r")
             for l in bl.readlines():
                 if l.startswith("#"):
                     # ignore comments
